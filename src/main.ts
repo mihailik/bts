3ba5550da5c2772d702b32d0c16ebabace554d4d
Oleg Mihailik <mihailik@gmail.com>
2013-10-12 00:05:22 +0100

    Grunt can go grunt itself. Node.js API is good enough.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-declare var brackets;
-declare var define;
+/// <reference path='typings/typescriptServices.d.ts' />
+
+declare var brackets, define;
 define(function (require, exports, module) {
   var AppInit = brackets.getModule("utils/AppInit");
   var CodeHintManager = brackets.getModule("editor/CodeHintManager");
   var Async = brackets.getModule("utils/Async");
   var StringUtils = brackets.getModule("utils/StringUtils");
 
+  
+  
 })
