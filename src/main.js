8dbd3795b948f71c857be7718ba42884c039e494
Oleg Mihailik <mihailik@gmail.com>
2013-10-12 00:06:49 +0100

    Output goes in the root.

--- src/main.js
+++
@@ -1,8 +0,0 @@
-/// <reference path='typings/typescriptServices.d.ts' />
-define(function (require, exports, module) {
-    var AppInit = brackets.getModule("utils/AppInit");
-    var CodeHintManager = brackets.getModule("editor/CodeHintManager");
-    var Async = brackets.getModule("utils/Async");
-    var StringUtils = brackets.getModule("utils/StringUtils");
-});
-//# sourceMappingURL=main.js.map
